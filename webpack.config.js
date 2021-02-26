72c7eabcfea00114800f0da3530ad7d523667393
Andres <castroandresrm@gmail.com>
2021-02-26 18:01:45 -0400

    Creamos un archivo de estilos de forma global

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,27 @@
 
 const HtmlWebpackPlugin = require('html-webpack-plugin'); 
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 
 module.exports = {
     mode: 'development',
+    optimization:{
+        minimizer: [new OptimizeCssAssetsPlugin()]
+    },
     module: {
         rules: [
             {
                 test: /\.css$/,
+                exclude: /styles\.css$/,
                 use:[
                     'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /styles\.css$/,
+                use:[
+                    MiniCssExtractPlugin.loader,
                     'css-loader'
                 ]
             },
@@ -28,5 +41,9 @@ module.exports = {
             template: './src/index.html',
             filename: './index.html'
         }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css',
+            ignoreOrder: false
+        })
     ],
 }
